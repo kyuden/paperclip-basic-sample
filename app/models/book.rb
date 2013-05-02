aee5226ed21e45a55ae6b0521bb5ba66ca431b36
Kyuden <msmsms.um@gmail.com>
2013-05-03 08:03:48 +0900

    Fix picture path

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ActiveRecord::Base
   attr_accessible :price, :publish, :title, :avatar
-  has_attached_file :avatar, styles: {midium: "300x300#", thumb: "`100x100#" }, default_url: "/system/books/:style/"
+  has_attached_file :avatar, styles: {midium: "300x300#", thumb: "100x100#" }, 
+  path: "#{Rails.root}/public/system/:class/:id/:attachment/:style.:extension",
+  url: "/system/:class/:id/:attachment/:style.:extension"
 
   validates_attachment :avatar, presence: true,
   content_type: { content_type: ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png'] }, size: { less_than: 3.megabytes }
